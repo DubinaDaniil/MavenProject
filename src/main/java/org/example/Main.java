79cbfdf9497ab50ceac1a41faca8fae5ace24948
Daniil <d.dubina@mobidev.biz>
2023-11-11 12:47:13 +0200

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * Created ${DATE} ${TIME}
+ *
+ * @since 3.5.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
